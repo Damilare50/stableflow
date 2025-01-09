92c8cf40bdb03ad99f277098b9f327f7ef219820
Dami <damilarelamine@gmail.com>
2025-01-09 19:52:21 +0100

    chore: install event-emitter

--- config/Config.d.ts
+++ config/Config.d.ts
@@ -1,12 +1,12 @@
 /* tslint:disable */
 /* eslint-disable */
-declare module 'node-config-ts' {
+declare module "node-config-ts" {
   interface IConfig {
-    APP_PORT: string;
-    MONGO_URI: string;
-    PRIVY_APP_ID: string;
-    PRIVY_APP_SECRET: string;
+    APP_PORT: string
+    MONGO_URI: string
+    PRIVY_APP_ID: string
+    PRIVY_APP_SECRET: string
   }
-  export const config: Config;
-  export type Config = IConfig;
+  export const config: Config
+  export type Config = IConfig
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 import { config } from 'node-config-ts';
 import { ProfileModule } from './profile/profile.module';
 import { PaymentModule } from './payment/payment.module';
@@ -11,6 +12,7 @@ import { GeneralModule } from './general/general.module';
 @Module({
   imports: [
     MongooseModule.forRoot(config.MONGO_URI, { dbName: 'stableflow' }),
+    EventEmitterModule.forRoot({ wildcard: true, delimiter: '.' }),
     ProfileModule,
     PaymentModule,
     SubscriptionModule,
