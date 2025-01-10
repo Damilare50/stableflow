1595d67148e7113e436e76043182966d646eba1a
Dami <damilarelamine@gmail.com>
2025-01-11 00:36:46 +0100

    chore: fix health path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,9 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { AuthGuard } from './guards/auth.guard';
 
 @Controller()
 @ApiTags('')
 export class AppController {
-  @UseGuards(AuthGuard)
   @Get()
   getHello(): string {
     return 'Hello stableflow!';
